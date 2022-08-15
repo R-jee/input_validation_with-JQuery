20676894f6fc1e0529d1cd1f3cf0672bfeead92d
Rana Umair <rjee4631@gmail.com>
2022-08-15 11:07:48 +0500

    Update validation.js

--- validation.js
+++ validation.js
@@ -7,3 +7,9 @@ $("#close___model__btn__id_2").on('click', function() {
 jQuery('#feed-name-input-id').keyup(function() {
             this.value = this.value.replace(/[^0-9a-zA-Z\-\_]/g, '');
 });
+jQuery('#name').keyup(function() {
+            this.value = this.value.replace(/[^0-9a-zA-Z\s\_]/g, '');
+});
+jQuery('#phone').keyup(function() {
+            this.value = this.value.replace(/^\d{5}$/g, '');
+});
